342c35ed234ad9ea8d9071e3c5339f7d8a1942fe
ravi nandan saxena <saxenaravi23599@gmail.com>
2023-05-13 14:33:42 +0530

    deploy branch prod

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eshop-backend-bluebits.herokuapp.com/api/v1/'
+  apiUrl: 'https://angular-ecommerce-backend.vercel.app//api/v1/'
 };
